d6903d19810c0b0af56bc3c1f3a116b6f278466f
ifhu <fuhaoq@qq.com>
2022-03-05 14:46:35 +0800

    添加JPA

--- src/main/java/top/ifhu/api/Application.java
+++ src/main/java/top/ifhu/api/Application.java
@@ -2,14 +2,12 @@ package top.ifhu.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class ApiApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(ApiApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
